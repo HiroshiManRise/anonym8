b2821fcae0ab3705d86402df88470a80172d8008
teeknofil <teeknofil@gmail.com>
2017-11-07 08:04:53 -0500

    Update INSTALL.sh with new file and check programm already install

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,12 +1,49 @@
 #!/bin/bash
-# Install script for anonym8
+
+#!/bin/bash
+
+#=======================================================================
+#
+#          FILE:  INSTALL.sh
+#
+#         USAGE:  setup projet
+#
+#   DESCRIPTION:  Install script for anonym8
+# 	Copyright (C) 2016 Teeknofil
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  Contact teeknofil.dev@gmail.com for bug.
+#        AUTHOR:  Twitter: @HiroshimanRise#         
+#        Thanks to:
+#		teeknofil '@teeknofil'
+#       COMPANY:  Anonymous freelance.
+#       COMPANY:  Community Team Teeknofil.
+#       VERSION:  2.0
+#       CREATED:  
+#      REVISION:  07/11/2017 03:42:31 CEST-
+#=======================================================================
+
 
 	export BLUE='\033[1;94m'
 	export GREEN='\033[1;92m'
 	export RED='\033[1;91m'
 	export RESETCOLOR='\033[1;00m'
+	
+	if [ $(id -u) -ne 0 ]; then
+			echo "[!] This script must be run as root!" >&2
+			exit 1
+	fi
 
+	if [ -f usr/share/anonym8/banner.sh ] ; then
+		
+		/bin/bash usr/share/anonym8/banner.sh
+	fi
+	
+	echo -e "\n$YELLOW$BOLD"
 	echo -e "This script will install anonym8 on your computer...\n"
+	echo -e " Press enter for continue of CTRL C for cancel$RESET"
+	read
 	sleep 1
 	cp -Rf opt/anonym8 /opt/anonym8
 	sleep 1
@@ -14,58 +51,82 @@
 	sleep 1
 	cp -Rf etc/anonym8.sh /etc/init.d/anonym8.sh
 	sleep 1
-	cp -Rf usr/anonym8 /usr/bin/anonym8
 	sleep 1
-	cp -Rf usr/anON /usr/bin/anON
+	cp -Rf usr/bin/* /usr/bin/
 	sleep 1
-	cp -Rf usr/anOFF /usr/bin/anOFF
+	mkdir /usr/share/anonym8	
+	cp -Rf usr/share/anonym8/* /usr/share/anonym8/
+	## Check already program installing for speed setting
+	apt-get update --fix-missing
 	sleep 1
-	apt-get install tor macchanger resolvconf dnsmasq polipo privoxy tor-arm libnotify-bin curl bleachbit
+	if [ ! -f /usr/sbin/virt-what ]; then
+		apt-get install -y virt-what
+	fi
 	sleep 1
-	cp -Rf polipo/config /etc/polipo/config
+	if [ ! -f /usr/bin/bleachbit ]; then
+	sleep 1	apt-get install -y bleachbit
+	fi
 	sleep 1
-	cp -Rf privoxy/config /etc/privoxy/config
+	if [ ! -f /usr/bin/proxychains ]; then
+		apt-get install -y proxychains
+	fi
 	sleep 1
-	cp -Rf tor/torrc /etc/tor/torrc
+	if [ ! -f /usr/bin/proxychains ]; then
+		apt-get install -y pwnat
+	fi
 	sleep 1
-	chmod +x /etc/init.d/anonym8.sh /usr/bin/anonym8 /usr/bin/anON /usr/bin/anOFF /opt/anonym8/anonym8-gui
+	if [ ! -f /usr/bin/macchanger  ]; then
+		apt-get install -y macchanger 
+	fi
 	sleep 1
-	echo -e "\n	 anonym8 (v 1.0) Usage Ex:\n
-	$RED anON$BLUE  =>$GREEN automated protection [ON]
-	$RED anOFF$BLUE =>$GREEN automated protection$RED [OFF]\n
-	$RED ADVANCED COMMANDS LIST:\n
-	$RED┌──[$GREEN$USER$RED@$BLUE`hostname`$RED]─[$GREEN$PWD$RED]
-	$RED└──╼ $GREEN"anonym8" $RED{$GREEN"start"$RED|$GREEN"stop"$RED|$GREEN"change"$RED|$GREEN"status..."$RED}\n
-	$BLUE----[ Tor Tunneling related features ]----
-	$RED anonym8 start$BLUE            =>$GREEN Start Tor Tunneling	  
-	$RED anonym8 stop$BLUE             =>$GREEN Stop Tor Tunneling
-	$RED anonym8 change$BLUE           =>$GREEN Changes identity restarting TOR
-	$RED anonym8 status$BLUE           =>$GREEN Tor Tunneling Status\n
-	$BLUE----[ IP related features ]----
-	$RED anonym8 status_ip$BLUE        =>$GREEN IP status\n
-	$BLUE----[ I2P related features ]----
-	$RED anonym8 start_i2p$BLUE        =>$GREEN Start i2p services
-	$RED anonym8 stop_i2p$BLUE         =>$GREEN Stop i2p services
-	$RED anonym8 status_i2p$BLUE       =>$GREEN i2p status\n
-	$BLUE----[ privoxy related features ]----
-	$RED anonym8 start_privoxy$BLUE    =>$GREEN Start privoxy services
-	$RED anonym8 stop_privoxy$BLUE     =>$GREEN Stop privoxy services
-	$RED anonym8 status_privoxy$BLUE   =>$GREEN privoxy status\n
-	$BLUE----[ polipo related features ]----
-	$RED anonym8 start_polipo$BLUE     =>$GREEN Start polipo services
-	$RED anonym8 stop_polipo$BLUE      =>$GREEN Stop polipo services
-	$RED anonym8 status_polipo$BLUE    =>$GREEN Polipo status\n
-	$BLUE----[ macchanger related features ]----
-	$RED anonym8 start_mac$BLUE        =>$GREEN Start macchanger services
-	$RED anonym8 stop_mac$BLUE         =>$GREEN Stop macchanger services
-	$RED anonym8 status_mac$BLUE       =>$GREEN macchanger status\n
-	$BLUE----[ arm related features ]----
-	$RED anonym8 start_arm$BLUE        =>$GREEN Start Monitoring Anonymizing Relay (arm)\n
-	$BLUE----[ wipe related features ]----
-	$RED anonym8 wipe$BLUE             =>$GREEN cache, RAM & swap-space cleaner\n
-	$BLUE----[ hostname related features ]----
-	$RED anonym8 change_hostname$BLUE  =>$GREEN Randomly Spoofing Hostname
-	$RED anonym8 restore_hostname$BLUE =>$GREEN Restore Default Hostname
-	$RED anonym8 status_hostname$BLUE  =>$GREEN Show Current Hostname\n
-
-	$RESETCOLOR"
+	if [ ! -f /sbin/resolvconf  ]; then
+		apt-get install -y resolvconf  
+	fi
+	sleep 1
+	if [ ! -f /usr/sbin/dnsmasq  ]; then
+		apt-get install -y dnsmasq   
+	fi
+	sleep 1
+	if [ ! -f /usr/bin/polipo   ]; then
+		apt-get install -y polipo    
+	fi
+	sleep 1
+	if [ ! -f /usr/sbin/privoxy   ]; then
+		apt-get install -y privoxy     
+	fi
+	sleep 1
+	if [ ! -f /usr/share/python/runtime.d/tor-arm.rtupdate  ]; then
+		apt-get install -y tor-arm     
+	fi
+	sleep 1
+	if [ ! -f /usr/bin/libnotify-bin   ]; then
+		apt-get install -y libnotify-bin     
+	fi
+	sleep 1
+	if [ ! -f /usr/bin/curl    ]; then
+		apt-get install -y curl      
+	fi
+	if [ ! -f /usr/sbin/tor  ]; then			
+		apt-get install tor
+			
+	fi
+	service tor restart
+	## End APT
+	
+	cp -Rf etc/polipo/config /etc/polipo/config
+	sleep 1
+	cp -Rf etc/privoxy/config /etc/privoxy/config
+	sleep 1
+	cp -Rf etc/tor/torrc /etc/tor/torrc
+	sleep 1
+	chmod +x /etc/init.d/anonym8.sh /usr/bin/anonym8 /usr/bin/iptables-tor-router /usr/bin/iptables-flush /usr/bin/transparent-proxy /usr/bin/anON /usr/bin/anOFF /opt/anonym8/anonym8-gui /usr/share/anonym8/*
+	sleep 1
+	# USAGE ####
+	updatedb
+	if [ -f $(which helpers.sh) ] ; then
+		
+		/bin/bash /bin/bash usr/share/anonym8/ helpers.sh
+	fi
+	
+echo -e $RESETCOLOR
+exit 0
