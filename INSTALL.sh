e28634b7ee06c73a998dab0ee7987422f40a96d2
teeknofil <teeknofil@gmail.com>
2017-11-07 12:27:49 -0500

    Fix Bux

--- INSTALL.sh
+++ INSTALL.sh
@@ -41,92 +41,122 @@
 	fi
 	
 	echo -e "\n$YELLOW$BOLD"
-	echo -e "This script will install anonym8 on your computer...\n"
+	echo -e " This script will install anonym8 on your computer...\n"
 	echo -e " Press enter for continue of CTRL C for cancel$RESET"
 	read
-	sleep 1
-	cp -Rf opt/anonym8 /opt/anonym8
-	sleep 1
+	sleep 0.5
+	echo -e "\a"
+	chmod +x /etc/init.d/anonym8.sh usr/bin/* /opt/anonym8/anonym8-gui /usr/share/anonym8/*
+	sleep 0.5
+	
+	cp -Rf opt/anonym8 /opt/
+	cp -Rf	var/lib/dbus/machine-id /var/lib/dbus/
+	sleep 0.5
+	
 	cp -Rf anonym8.desktop /usr/share/applications
-	sleep 1
+	sleep 0.5
+	
 	cp -Rf etc/anonym8.sh /etc/init.d/anonym8.sh
-	sleep 1
-	sleep 1
+	sleep 0.5
+	
 	cp -Rf usr/bin/* /usr/bin/
-	sleep 1
-	mkdir /usr/share/anonym8	
+	sleep 0.5
+	
+	mkdir -p /usr/share/anonym8
 	cp -Rf usr/share/anonym8/* /usr/share/anonym8/
+	
 	## Check already program installing for speed setting
-	apt-get update --fix-missing
-	sleep 1
+	apt-get update --fix-missing 
+	sleep 0.5
+	
+	## Intall POLIPO
+	if [ ! -f /usr/bin/polipo ]; then
+		apt-get install -y polipo
+		cp -Rf etc/polipo/config /etc/polipo/	
+	else		
+		cp -Rf etc/polipo/config /etc/polipo/    
+	fi
+	sleep 0.5
+
+	## Intall Privoxy
+	if [ ! -f /usr/sbin/privoxy   ]; then
+		apt-get install -y privoxy
+		cp -Rf etc/privoxy/config /etc/privoxy/  
+	else
+		cp -Rf etc/privoxy/* /etc/privoxy/       
+	fi
+	sleep 0.5
+	
+	## Intall Tor
+	if [ ! -f /usr/sbin/tor ]; then	
+		apt-get install -y tor
+		cp -Rf etc/tor/* /etc/tor/	
+	else
+		cp -Rf etc/tor/* /etc/tor/
+	fi
+	sleep 0.5
+	
 	if [ ! -f /usr/sbin/virt-what ]; then
 		apt-get install -y virt-what
 	fi
-	sleep 1
+	sleep 0.5
+
 	if [ ! -f /usr/bin/bleachbit ]; then
-	sleep 1	apt-get install -y bleachbit
+		apt-get install -y bleachbit
 	fi
-	sleep 1
+	sleep 0.5
+	
 	if [ ! -f /usr/bin/proxychains ]; then
 		apt-get install -y proxychains
 	fi
-	sleep 1
+	sleep 0.5
+	
 	if [ ! -f /usr/bin/proxychains ]; then
 		apt-get install -y pwnat
 	fi
-	sleep 1
+	sleep 0.5
+	
 	if [ ! -f /usr/bin/macchanger  ]; then
 		apt-get install -y macchanger 
 	fi
-	sleep 1
+	sleep 0.5
+	
 	if [ ! -f /sbin/resolvconf  ]; then
 		apt-get install -y resolvconf  
 	fi
-	sleep 1
+	sleep 0.5
+	
+	#  Install dnsmasq
 	if [ ! -f /usr/sbin/dnsmasq  ]; then
 		apt-get install -y dnsmasq   
 	fi
-	sleep 1
-	if [ ! -f /usr/bin/polipo   ]; then
-		apt-get install -y polipo    
-	fi
-	sleep 1
-	if [ ! -f /usr/sbin/privoxy   ]; then
-		apt-get install -y privoxy     
-	fi
-	sleep 1
+	sleep 0.5
+		
 	if [ ! -f /usr/share/python/runtime.d/tor-arm.rtupdate  ]; then
 		apt-get install -y tor-arm     
 	fi
-	sleep 1
+	sleep 0.5
+	
 	if [ ! -f /usr/bin/libnotify-bin   ]; then
 		apt-get install -y libnotify-bin     
 	fi
-	sleep 1
+	sleep 0.5
+	
 	if [ ! -f /usr/bin/curl    ]; then
 		apt-get install -y curl      
 	fi
-	if [ ! -f /usr/sbin/tor  ]; then			
-		apt-get install tor
-			
-	fi
+	sleep 0.5	
+	
 	service tor restart
+	
 	## End APT
 	
-	cp -Rf etc/polipo/config /etc/polipo/config
-	sleep 1
-	cp -Rf etc/privoxy/config /etc/privoxy/config
-	sleep 1
-	cp -Rf etc/tor/torrc /etc/tor/torrc
-	sleep 1
-	chmod +x /etc/init.d/anonym8.sh /usr/bin/anonym8 /usr/bin/iptables-tor-router /usr/bin/iptables-flush /usr/bin/transparent-proxy /usr/bin/anON /usr/bin/anOFF /opt/anonym8/anonym8-gui /usr/share/anonym8/*
-	sleep 1
+	echo -e "\n"
 	# USAGE ####
 	updatedb
 	if [ -f $(which helpers.sh) ] ; then
-		
-		/bin/bash /bin/bash usr/share/anonym8/ helpers.sh
+		/bin/bash usr/share/anonym8/helpers.sh
 	fi
-	
-echo -e $RESETCOLOR
+
+echo -e "$RESETCOLOR"
 exit 0
